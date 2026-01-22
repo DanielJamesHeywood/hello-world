f4312c58415497845e09de0d2919f3c2338814a6
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-22 12:36:50 +0000

    Initial Commit

---
+++ hello world/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hello world
+//
+//  Created by Daniel James Heywood on 22/01/2026.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
