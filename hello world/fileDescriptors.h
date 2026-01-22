c34ec6691a0bfad1665deedb0681dc5ae39f2105
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-22 13:20:53 +0000

    Provided a preprocessor macro for standardOutput.

---
+++ hello world/fileDescriptors.h
@@ -0,0 +1,2 @@
+
+#define standardOutput 1
--- hello world/main.c
+++ hello world/main.c
@@ -1,6 +1,7 @@
+#include "fileDescriptors.h"
 #include "systemCalls.h"
 
 int main(int argc, const char * argv[]) {
-    write(1, "Hello, World!\n", 14);
+    write(standardOutput, "Hello, World!\n", 14);
     return 0;
 }
