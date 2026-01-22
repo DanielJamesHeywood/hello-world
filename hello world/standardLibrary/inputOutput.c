e2873e40550f864b53465a24f13502059a0b81cd
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-22 16:58:43 +0000

    Extracted calculation of string length to string.

--- hello world/standardLibrary/inputOutput.c
+++ hello world/standardLibrary/inputOutput.c
@@ -2,10 +2,8 @@
 
 #include "system/systemCalls.h"
 
+#include "string.h"
+
 long writeString(int fileDescriptor, const char* string) {
-    unsigned long numberOfBytes = 0;
-    while (string[numberOfBytes] != '\0') {
-        numberOfBytes += 1;
-    }
-    return write(fileDescriptor, string, numberOfBytes);
+    return write(fileDescriptor, string, stringLength(string));
 }
---
+++ hello world/standardLibrary/string.c
@@ -0,0 +1,9 @@
+#include "String.h"
+
+unsigned long stringLength(const char* string) {
+    unsigned long length = 0;
+    while (string[length] != '\0') {
+        length += 1;
+    }
+    return length;
+}
