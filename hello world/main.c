e09e52df6d1251b573e2212b067a5ef37b81d18e
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-01-22 16:02:43 +0000

    Properly prefix includes in main.

--- hello world/main.c
+++ hello world/main.c
@@ -1,6 +1,6 @@
-#include "exitStatuses.h"
-#include "fileDescriptors.h"
-#include "systemCalls.h"
+#include "system/exitStatuses.h"
+#include "system/fileDescriptors.h"
+#include "system/systemCalls.h"
 
 int main(int argumentCount, const char* argumentVector[]) {
     write(standardOutput, "Hello, World!\n", 14);
